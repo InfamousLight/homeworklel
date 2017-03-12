96bf0c8b8cd2a3c5e2d9b48c3917f1090a116a95
InfamousLight <immortalcross@gmail.com>
2017-03-12 15:50:40 -0500

    edit band page

--- resources/views/edit-band.blade.php
+++ resources/views/edit-band.blade.php
@@ -2,6 +2,28 @@
 
 @section('content')
     <div class="container">
-        Edit band here
+        <h1>Edit Band</h1><br><br>
+        <form method="POST" action="{{ action('BandController@edit') }}" role="form">
+            {!! csrf_field() !!}
+            <div class="form-group">
+                <label for="band-name">Band Name</label>
+                <input class="form-control" id="band-name" name="band-name" placeholder="Enter Band Name" value={{$band->name}}>
+            </div>
+            <div class="form-group">
+                <label for="website">Website</label>
+                <input class="form-control" id="website" name="website" placeholder="Enter Bands Website" value={{$band->website}}>
+            </div>
+            <div class="form-group">
+                <label for="start-date">Start Date</label>
+                <input class="form-control" id="start-date" name="start-date" placeholder="Enter Start Date" value={{$band->start_date}}>
+            </div>
+            <div class="form-group">
+                <label for="active">Active</label>
+                <input class="form-control" id="active" name="active" placeholder="Enter 1 or 0 for Active" value={{$band->still_active}}>
+            </div>
+            <br><br>
+            <input type="hidden" name="bandId" value="{{$band->id}}" />
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
 @endsection
