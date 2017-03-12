82bc927652f1fc6327f8ae030e1bd5c8a76c912d
InfamousLight <immortalcross@gmail.com>
2017-03-11 19:41:53 -0600

    album seeder

--- database/seeds/AlbumTableSeeder.php
+++ database/seeds/AlbumTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class AlbumTableSeeder extends Seeder
 {
@@ -11,6 +12,21 @@ class AlbumTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $bands = DB::table('Band')->select('id')->get();
+
+        foreach($bands as $band) {
+            for($i = 0; $i < rand(1, 5); $i++) {
+                DB::table('Album')->insert([
+                    'band_id' => $band->id,
+                    'name' => str_random(10),
+                    'recorded_date' => Carbon::now('America/Chicago')->subMonth(rand(1, 150))->toDateString(),
+                    'release_date' => Carbon::now('America/Chicago')->addMonth(rand(1, 12))->toDateString(),
+                    'number_of_tracks' => rand(1,100),
+                    'label' => str_random(5),
+                    'producer' => str_random(5) . ' ' . str_random(5),
+                    'genre' => 'rock'
+                ]);
+            }
+        }
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
         $this->call(BandTableSeeder::class);
-//        $this->call(AlbumTableSeeder::class);
+        $this->call(AlbumTableSeeder::class);
         Model::reguard();
     }
 }
