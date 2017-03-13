bef81498354b368c09e66505cde9a0e95e04ad27
InfamousLight <immortalcross@gmail.com>
2017-03-12 20:39:54 -0500

    required fields

--- resources/views/edit-album.blade.php
+++ resources/views/edit-album.blade.php
@@ -9,7 +9,7 @@
             {!! csrf_field() !!}
             <div class="form-group">
                 <label for="band-id">Select Band</label>
-                <select class="form-control" id="band-id" name="band-id">
+                <select class="form-control" id="band-id" name="band-id" required>
                     @foreach ($bands as $band)
                         @if($band->id == $belongsToBand->id)
                             <option value={{$band->id}} selected>{{$band->name}}</option>
@@ -21,7 +21,7 @@
             </div>
             <div class="form-group">
                 <label for="album-name">Album Name</label>
-                <input class="form-control" id="album-name" name="album-name" placeholder="Enter Band Name" value="{{$album->name}}">
+                <input class="form-control" id="album-name" name="album-name" placeholder="Enter Band Name" value="{{$album->name}}" required>
             </div>
             <div class="form-group">
                 <label for="recorded-date">Recorded Date</label>
@@ -33,7 +33,7 @@
             </div>
             <div class="form-group">
                 <label for="number-of-tracks">Number of Tracks</label>
-                <input class="form-control" id="number-of-tracks" name="number-of-tracks" placeholder="Enter Number of Tracks" value={{$album->number_of_tracks}}>
+                <input type="number" class="form-control" id="number-of-tracks" name="number-of-tracks" placeholder="Enter Number of Tracks" value={{$album->number_of_tracks}}>
             </div>
             <div class="form-group">
                 <label for="label">Label</label>
